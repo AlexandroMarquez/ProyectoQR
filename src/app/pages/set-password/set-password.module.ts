76a42ac6b4aba2232218a088909d92a0905dcb90
aleksandrovno <al.marquezm@duocuc.cl>
2023-11-08 14:16:06 -0300

    .

--- src/app/pages/set-password/set-password.module.ts
+++ src/app/pages/set-password/set-password.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 import { SetPasswordPageRoutingModule } from './set-password-routing.module';
@@ -13,6 +13,7 @@ import { SetPasswordPage } from './set-password.page';
     CommonModule,
     FormsModule,
     IonicModule,
+    ReactiveFormsModule,
     SetPasswordPageRoutingModule
   ],
   declarations: [SetPasswordPage]
--- src/app/pages/set-password/set-password.page.ts
+++ src/app/pages/set-password/set-password.page.ts
@@ -9,11 +9,20 @@ import { Router } from '@angular/router';
   templateUrl: './set-password.page.html',
   styleUrls: ['./set-password.page.scss'],
 })
-export class SetPasswordPage implements OnInit {
+export class SetPasswordPage {
 
-  constructor() { }
+  setForm: FormGroup; // Declarar la propiedad 'setForm' aquí
 
-  ngOnInit() {
+  constructor(private fb: FormBuilder, private alertController: AlertController, private router: Router) {
+    this.setForm = this.fb.group({
+      password: ['', [Validators.required]],
+    });
   }
 
+  async setPassword() {
+    const f = this.setForm.value;
+    const userResult = await Preferences.get({ key: 'usuarioData' });
+    console.log(f);
+    console.log(userResult);
+  }
 }
